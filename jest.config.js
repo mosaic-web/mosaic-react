f6cd090379a66eac49d4c3ea70dc006bc69f1c0c
Oleh Kutsenko <inthedark1225@gmail.com>
2022-03-26 21:21:27 +0800

    eslint, pre-commit and jest configuration

---
+++ jest.config.js
@@ -0,0 +1,13 @@
+/** @type {import('ts-jest/dist/types').InitialOptionsTsJest} */
+module.exports = {
+  preset: 'ts-jest',
+  testEnvironment: 'node',
+  coverageThreshold: {
+    global: {
+      branches: 90,
+      functions: 90,
+      lines: 90,
+      statements: 90,
+    },
+  },
+};
